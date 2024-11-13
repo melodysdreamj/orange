349edec2b1208c938443c9b91cd5909a56bedb36
june lee <melodysdreamj@gmail.com>
2024-11-13 21:20:00 +0900

    support wasm

--- lib/src/map.dart
+++ lib/src/map.dart
@@ -3,24 +3,25 @@ import 'dart:async';
 // import 'save_to_file/device.dart';
 import 'save_to_file/device.dart'
     if (dart.library.html) 'save_to_file/web.dart'
+    if (dart.library.js) 'save_to_file/web.dart'
     if (dart.library.cli) 'save_to_file/cli.dart';
 
 class OrangeMap {
   static Map<String, dynamic>? _map;
 
   static void set(String key, dynamic value) {
-    if(_map == null) throw Exception('OrangeMap not initialized');
+    if (_map == null) throw Exception('OrangeMap not initialized');
     _map![key] = value;
     DeviceStorage().set(key, value);
   }
 
   static dynamic get(String key) {
-    if(_map == null) throw Exception('OrangeMap not initialized');
+    if (_map == null) throw Exception('OrangeMap not initialized');
     return _map![key];
   }
 
   static void remove(String key) {
-    if(_map == null) throw Exception('OrangeMap not initialized');
+    if (_map == null) throw Exception('OrangeMap not initialized');
     _map!.remove(key);
     DeviceStorage().remove(key);
   }
